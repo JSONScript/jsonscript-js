8360c560eb2954d835ed773567684edf75723024
Evgeny Poberezkin <e.poberezkin@me.com>
2016-02-19 20:40:46 +0000

    move part of $if tests to jsonscript-test-suite

--- spec/$if.spec.js
+++ spec/$if.spec.js
@@ -27,44 +27,6 @@ describe('$if instruction - conditional evaluation', function() {
     elseRouter: 'router2'
   };
 
-  it('should return the result of evaluation of script in $then or in $else', function() {
-    return Promise.all([
-      test({ $if: true, $then: 'foo', $else: 'bar' }, 'foo'),
-      test({ $if: false, $then: 'foo', $else: 'bar' }, 'bar'),
-      test({ $if: false, $then: 'foo' }, null),
-      test({ $if: { $data: '/cond1' }, $then: 'foo', $else: 'bar' }, 'foo'),
-      test({ $if: { $data: '/cond2' }, $then: 'foo', $else: 'bar' }, 'bar'),
-      test({ $if: { $data: '/cond2' }, $then: 'foo' }, null),
-      test({
-        $if: true,
-        $then: { $data: '/then' },
-        $else: { $data: '/else' }
-      }, 'foo'),
-      test({
-        $if: false,
-        $then: { $data: '/then' },
-        $else: { $data: '/else' }
-      }, 'bar'),
-      test({
-        $if: false,
-        $then: { $data: '/then' }
-      }, null),
-      test({
-        $if: { $data: '/cond1' },
-        $then: { $data: '/then' },
-        $else: { $data: '/else' }
-      }, 'foo'),
-      test({
-        $if: { $data: '/cond2' },
-        $then: { $data: '/then' },
-        $else: { $data: '/else' }
-      }, 'bar'),
-      test({
-        $if: { $data: '/cond2' },
-        $then: { $data: '/then' }
-      }, null)
-    ]);
-  });
 
   it('should allow for $if/$then/$else to be asynchronous', function() {
     return Promise.all([
@@ -113,30 +75,9 @@ describe('$if instruction - conditional evaluation', function() {
     ]);
   });
 
-  it('should allow for $if/$then/$else to be any script', function() {
-    return Promise.all([
-      test({
-        $if: { $data: '/cond1' },
-        $then: { $exec: 'router1', $method: 'get', $args: { path: '/resource' } },
-        $else: { $exec: 'router2', $method: 'get', $args: { path: '/resource' } }
-      }, 'you requested /resource from router1'),
-      test({
-        $exec: {
-          $if: { $data: '/cond1' },
-          $then: { $data: '/thenRouter' },
-          $else: { $data: '/elseRouter' }
-        },
-        $method: 'get',
-        $args: { path: '/resource' }
-      }, 'you requested /resource from router1')
-    ]);
-  });
 
   it('should throw error if $if is not boolean', function() {
     return Promise.all([
-      shouldBeError(js.evaluate({ $if: 1, $then: 'foo' }), 'validation failed', ['should be boolean']),
-      shouldBeError(js.evaluate({ $if: { $data: '/notBoolean' }, $then: 'foo' }, data),
-        'validation failed', ['should be boolean']),
       shouldBeError(js.evaluate({ $if: { $exec: 'async', $args: 1 }, $then: 'foo' }, data),
         'validation failed', ['should be boolean']),
       shouldBeError(js.evaluate({ $if: { $data: { $exec: 'async', $args: '/notBoolean' } }, $then: 'foo' }, data),
